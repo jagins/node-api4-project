923138f8cae648804eeff5bbb0c72b47047cc1e6
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-13 15:26:55 -0600

    working on my own api songsDatabase is static

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const songRouter = require('./songs/songRouter');
+const server = express();
+server.use(express.json());
+
+server.use('/api/songs', songRouter);
+
+server.get('/', (req, res) =>
+{
+    res.json({message: 'api is working'});
+})
+
+
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ songs/songsDatabase.js
@@ -0,0 +1,38 @@
+const songs = [
+    {
+        id: 1,
+        title: 'Sharp Dressedman',
+        artist: 'ZZ Top',
+        albmum: 'Eliminator'
+    },
+    {
+        id: 2,
+        title: 'Killing In The Name',
+        artist: 'Rage Against The Machine',
+        albmum: 'Rage Against The Machine'
+    },
+    {
+        id: 3,
+        title: 'You Give Love A Bad Name',
+        artist: 'Bon Jovi',
+        albmum: 'Cross Road'
+    },
+    {
+        id: 4,
+        title: 'Thunderstruck',
+        artist: 'AC/DC',
+        albmum: 'The Razors Edge'
+    },
+    {
+        id: 5,
+        title: 'Master Of Puppets',
+        artist: 'Metalica',
+        albmum: 'Rage Against The Machine'
+    },
+    {
+        id: 6,
+        title: 'Iron Man',
+        artist: 'Black Sabbath',
+        albmum: 'Parnoid'
+    }
+]
